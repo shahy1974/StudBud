874639b983a2cc12d7376ff6c5992d18865731c0
shahy1974 <shahinazfkh@gmail.com>
2025-04-27 01:38:21 +0300

    Add files via upload

--- app1.py
+++ app1.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, jsonify, redirect, url_for, session, send_from_directory
+from flask import Flask, render_template, request, jsonify, redirect, url_for, session
 from werkzeug.security import generate_password_hash, check_password_hash
 import requests
 import os
